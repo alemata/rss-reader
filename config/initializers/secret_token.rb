074f38d6cf8cba14cd5faf08656148c19e644572
alemata <amataloni@gmail.com>
2013-09-27 23:41:33 -0300

    Initial application commit. Application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RssReader::Application.config.secret_key_base = '27a6b71188caecb6eafef18a2679329b7dc479329500958b0a0d631dca80431b845b4b08fcc2b52f8081275ca711ee7819c9d24c5f11ac56bed9ce75b7e9fdec'
